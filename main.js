04e294be2b98c6095e68b3146d59914e08f18198
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-05-14 14:47:38 +0500

    This is  Assignment Question 6
    This is  Assignment Question 6

---
+++ main.js
@@ -0,0 +1,14 @@
+// 6. Anonymous functions:
+// .  Assign a function expression to a variable, with one parameters that outputs the provided arguments to the console.Console
+// .  Pass an argument into the function.
+// .  Create the same function as a normal function declaration.
+var func1 = function (str) {
+    console.log(str);
+};
+func1("Hello World");
+//Normal function declaration
+function func2(str) {
+    console.log(str);
+}
+;
+func2("Hello Bisma");
---
+++ main.ts
@@ -0,0 +1,16 @@
+// 6. Anonymous functions:
+
+// .  Assign a function expression to a variable, with one parameters that outputs the provided arguments to the console.Console
+// .  Pass an argument into the function.
+// .  Create the same function as a normal function declaration.
+
+let func1= function (str: string) {
+    console.log(str);
+};
+func1("Hello World")
+
+//Normal function declaration
+function  func2 (str: string) {
+    console.log(str);
+};
+func2("Hello Bisma")
